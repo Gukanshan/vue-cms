3351d7645f764396691764c085c2b590cbb005c4
Gu_Sir <vortexg@163.com>
2023-03-05 22:52:55 +0800

    init repository

---
+++ src/views/main/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <h2>
+      main: {{ counterStore.counter }} -- {{ counterStore.doubleCounter }}
+    </h2>
+    <button @click="changeCounter">修改counter</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import useCounterStore from '@/store/counter'
+const counterStore = useCounterStore()
+
+function changeCounter() {
+  counterStore.changeCounterAction(200)
+}
+</script>
+
+<style lang="less" scoped>
+.main {
+  color: black;
+}
+</style>
